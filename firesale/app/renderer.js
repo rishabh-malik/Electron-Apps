8ebed66b8d2f93cc6dbacaf55f6ea7eefb02e4a4
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-10 14:30:37 +0530

    render function

--- firesale/app/renderer.js
+++ firesale/app/renderer.js
@@ -1,3 +1,4 @@
+//convert markdown to html
 const marked = require('marked');
 
 const markdownView = document.querySelector('#markdown');
@@ -7,3 +8,13 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+
+//render function
+const renderMarkdownToHtml=(markdown)=>{
+    htmlView.innerHTML=marked(markdown,{sanitize:true});
+}
+
+//adding event listener
+markdownView.addEventListener('keyup',(event)=>{
+    renderMarkdownToHtml(event.target.value);
+})
