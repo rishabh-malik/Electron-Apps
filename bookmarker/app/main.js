92b0cb0b4b2ed84d938e4251694fece2c792792e
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-10 12:05:22 +0530

    working on mainWindow

--- bookmarker/app/main.js
+++ bookmarker/app/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const {app,BrowserWindow} = require('electron');
+
+//when app gets ready
+app.on('ready',()=>{
+    //console.log('the app is ready!');
+    const mainWindow=new BrowserWindow({
+        width:500,
+        height:600,
+        titleBarStyle:'hidden-inset',
+        show:false
+    });
+
+    mainWindow.once('ready-to-show',()=>{
+        mainWindow.show();
+    });
+
+    //load our own page in the window
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
