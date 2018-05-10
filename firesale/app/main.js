054f3b5e5b28ace75acfbbbfa0504c29e4319f7b
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-10 14:46:50 +0530

    getting contents of file

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -1,6 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs=require('fs');
 
 let mainWindow = null;
+
+//opening a file since dialog is only available in the main
+const getFileFromUserSelection=()=>{
+  const files=dialog.showOpenDialog(mainWindow,{
+    properties:['openFile'],
+    filters:[
+      {name:'Text Files',extensions:['txt','text']},
+      {name:'Markdown Files',extensions:['md']}
+    ]
+  });
+
+  if(!files) return;
+
+  const file=files[0];
+  const content=fs.readFileSync(file).toString();
+  console.log(content);
+}
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ show: false });
