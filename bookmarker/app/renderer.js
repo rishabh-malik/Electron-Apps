2fae6ba7dc6df3938ed9a6eec87158dadb1ad0b1
rishabh-malik <rishabhmalik249@gmail.com>
2018-05-10 12:52:16 +0530

    adding bookmarks

--- bookmarker/app/renderer.js
+++ bookmarker/app/renderer.js
@@ -1,6 +1,8 @@
 const newLinkUrl=document.querySelector('#new-link-url');
 const newLikSubmit=document.querySelector('.new-link-form--submit');
 const newLinkForm=document.querySelector('.new-link-form');
+const linkTemplate=document.querySelector('#link-template');
+const linkSection=document.querySelector('.links');
 
 //to see if the url is valid or not
 newLinkUrl.addEventListener('keyup',()=>{
@@ -15,6 +17,19 @@ const parseResponse=(text)=>parser.parseFromString(text,'text/html');
 //get the title of the webpage
 const findTitle=(nodes)=> nodes.querySelector('title').textContent;
 
+//adding to page
+const addToPage=({title,url})=>{
+    const newLink=linkTemplate.content.cloneNode(true);
+    const titleElement=newLink.querySelector('.link--title');
+    const urlElement=newLink.querySelector('.link--url');
+
+    titleElement.textContent=title;
+    urlElement.href=url;
+    urlElement.textContent=url;
+
+    linkSection.appendChild(newLink);
+    return {title,url};
+};
 
 //on form submit
 newLinkForm.addEventListener('submit',()=>{
@@ -26,6 +41,10 @@ newLinkForm.addEventListener('submit',()=>{
     .then(response=>response.text())
     .then(parseResponse)
     .then(findTitle)
+    .then(title=>({
+        title,url
+    }))
+    .then(addToPage)
     .then(title=>console.log(title))
     .catch(error=>console.log(error));
 });
